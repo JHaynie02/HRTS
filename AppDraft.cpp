32c3eb607e224e8a44a2d3633137ad4eacc0a7cd
JHaynie02 <jmhh7d@umsystem.edu>
2022-04-25 17:44:26 -0500

    Added commenting for constructor and Handle func

--- AppDraft.cpp
+++ AppDraft.cpp
@@ -1,10 +1,12 @@
 #include "AppDraft.h"
 #include "AppFinished.h"
 
+// AppDraft Constructor with initialization list setting context to context_
 AppDraft::AppDraft(Applications* context) : context_(context) {};
 
+// Handle function to change application from draft to finished
 void AppDraft::Handle()
 {
-    std::cout << "Application in draft mode\n";
+    std::cout << "\nApplication now in draft mode\n";
     context_->setState(new AppFinished(context_));
 }
