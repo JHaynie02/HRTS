6f2707e89003a4cc02cf28922bb90a933e5bd47b
JHaynie02 <jmhh7d@umsystem.edu>
2022-04-26 19:20:38 -0500

    Tested some json input/output

--- main.cpp
+++ main.cpp
@@ -3,13 +3,32 @@
 
 int main()
 {
-    Interface &user = Interface::GetInstance();
-    user.InterfaceControl();
+    // Interface &user = Interface::GetInstance();
+    // user.InterfaceControl();
 
     // Applications applications;
     // applications.Handle();
     // applications.Handle();
     // applications.Handle();
     // applications.Handle();
+
+    std::string filePath = "pretty.json";
+    std::string response = "Jesse";
+    jsonf jsonfileWrite;
+    jsonfileWrite["Name"] = response;
+    std::ofstream file(filePath);
+    file << std::setw(3) << jsonfileWrite;
+    // std::cout << jsonfileWrite;
+    file.close();
+
+    // std::string readInString;
+    // jsonf jsonfileRead;
+    std::ifstream fileRead(filePath, std::ios::in);
+    std::cout << fileRead.rdbuf();
+    // // jsonfileRead = jsonf::parse(fileRead);
+    // // fileRead >> jsonfileRead;
+    // // std::cout << jsonfileRead;
+    // fileRead.close();
+
     return 0;
 }
