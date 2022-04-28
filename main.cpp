6c440814082abcff14e3e2defdbcca4af9fab62b
JHaynie02 <jmhh7d@umsystem.edu>
2022-04-27 19:55:42 -0500

    Added some json and time.h testing

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    // Interface &user = Interface::GetInstance();
-    // user.InterfaceControl();
+    Interface &user = Interface::GetInstance();
+    user.InterfaceControl();
 
     // Applications applications;
     // applications.Handle();
@@ -12,23 +12,32 @@ int main()
     // applications.Handle();
     // applications.Handle();
 
-    std::string filePath = "pretty.json";
-    std::string response = "Jesse";
-    jsonf jsonfileWrite;
-    jsonfileWrite["Name"] = response;
-    std::ofstream file(filePath);
-    file << std::setw(3) << jsonfileWrite;
-    // std::cout << jsonfileWrite;
-    file.close();
+    // std::string filePath = "pretty.json";
+    // std::string response = "Jesse";
+    // jsonf jsonfileWrite;
+    // time_t timeToday;
+    // time(&timeToday);
+    // // jsonfileWrite["Time"] = asctime(localtime(&timeToday));
+    // jsonfileWrite["Name"] = response;
+    // response = asctime(localtime(&timeToday));
+    // std::cout << response;
+    // response.erase(std::remove(response.begin(), response.end(), '\n'), response.end());
+    // jsonfileWrite["Time"] = response;
+    // // jsonfileWrite["Time"] = asctime(localtime(&timeToday));
+    // std::ofstream file(filePath);
+    // file << std::setw(3) << jsonfileWrite;
+    // // file << jsonfileWrite;
+    // std::cout << std::setw(3) << jsonfileWrite;
+    // file.close();
 
-    // std::string readInString;
-    // jsonf jsonfileRead;
-    std::ifstream fileRead(filePath, std::ios::in);
-    std::cout << fileRead.rdbuf();
-    // // jsonfileRead = jsonf::parse(fileRead);
-    // // fileRead >> jsonfileRead;
-    // // std::cout << jsonfileRead;
-    // fileRead.close();
+    // // // std::string readInString;
+    // // jsonf jsonfileRead;
+    // std::ifstream fileRead(filePath, std::ios::in);
+    // std::cout << fileRead.rdbuf();
+    // jsonfileRead = jsonf::parse(fileRead);
+    // // // fileRead >> jsonfileRead;
+    // std::cout << jsonfileRead;
+    // // fileRead.close();
 
     return 0;
 }
